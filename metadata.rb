38cc494016248fc58e7175db2c4aacdb2bc7eb9b
Jacob McCann <jacob.mccann2@target.com>
2017-05-26 08:52:26 -0500

    Add generated cookbook content

---
+++ metadata.rb
@@ -0,0 +1,11 @@
+name 'consul-docker'
+maintainer 'Jacob McCann'
+maintainer_email 'jacob.mccann2@target.com'
+license 'Apache-2.0'
+description 'Installs/Configures consul-docker'
+long_description 'Installs/Configures consul-docker'
+version '0.1.0'
+chef_version '>= 12.1' if respond_to?(:chef_version)
+
+source_url 'https://github.com/jmccann/consul-docker-cookbook'
+issues_url 'https://github.com/jmccann/consul-docker-cookbook/issues'
