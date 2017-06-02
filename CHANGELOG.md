214a92bcf7795623150959e2671530c117677a7b
Jacob McCann <jacob.mccann2@target.com>
2017-06-02 10:44:53 -0500

    Version 0.3.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+# 0.3.1
+* Fix loading/merging of secrets into `CONSUL_LOCAL_CONFIG`
+
 # 0.3.0
 * Add ability to load config items from vault
 
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'jacob.mccann2@target.com'
 license 'Apache-2.0'
 description 'Installs/Configures consul-docker'
 long_description 'Installs/Configures consul-docker'
-version '0.3.0'
+version '0.3.1'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
 source_url 'https://github.com/jmccann/consul-docker-cookbook'
