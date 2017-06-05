01bddce86e2a6a23816b264dc74adfba5dd29988
Jacob McCann <jacob.mccann2@target.com>
2017-06-05 09:52:45 -0500

    Have entrypoint be executable

--- recipes/default.rb
+++ recipes/default.rb
@@ -16,7 +16,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-cookbook_file '/root/entrypoint.sh'
+cookbook_file '/root/entrypoint.sh' do
+  mode '0755'
+end
 
 docker_image 'consul' do
   repo node['consul-docker']['repo']
